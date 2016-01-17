9bd93baace953e038db6390d040386a64407d787
Monte Thakkar <thakkarmanthan9@yahoo.com>
2016-01-16 23:23:22 -0800

    TMDB linked, presenting movies in table view

---
+++ Project1-FlickM/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Project1-FlickM
+//
+//  Created by Monte's Pro 13" on 1/16/16.
+//  Copyright © 2016 MonteThakkar. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Project1-FlickM/MoviesController.swift
+++ Project1-FlickM/MoviesController.swift
@@ -12,12 +12,41 @@ class MoviesController: UIViewController, UITableViewDelegate, UITableViewDataSo
 
     @IBOutlet weak var MoviesTableView: UITableView!
     
+    var movies: [NSDictionary]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         MoviesTableView.dataSource = self
         MoviesTableView.delegate = self
+        
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            self.movies = responseDictionary["results"] as! [NSDictionary]
+                            self.MoviesTableView.reloadData()
+                    }
+                }
+                
+        });
+        task.resume()
+        
+       
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,15 +55,20 @@ class MoviesController: UIViewController, UITableViewDelegate, UITableViewDataSo
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return movies?.count ?? 0
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath)
-      //  cell.textLabel?.text = indexPath.row
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
+        
+        let movie = movies![indexPath.row]
+            
+        cell.titleLabel.text = movie["title"] as! String
+        cell.overviewLabel.text = movie["overview"] as! String
 
-        cell.textLabel?.text = "row\(indexPath.row)"
-        print("row\(indexPath.row)")
+//        cell.textLabel?.text = "row\(indexPath.row)"
+//        print("row\(indexPath.row)")
+        
         return cell
     }
 
